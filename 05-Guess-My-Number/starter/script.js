ba1a193d01287a933cf4d8a3d3128153dd92bfa1
Neha Rajput <neharajputcoding@gmail.com>
2024-07-21 23:56:56 +0530

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-//Read the text content of a DOM element
-console.log(document.querySelector('.message').textContent);
+// //Read the text content of a DOM element
+// console.log(document.querySelector('.message').textContent);
 
-//Modify the text content of a DOM element
-document.querySelector('.message').textContent = "🥳Correct Number!🎉";
+// //Modify the text content of a DOM element
+// document.querySelector('.message').textContent = "🥳Correct Number!🎉";
 
-document.querySelector('.number').textContent = '23';
-document.querySelector('.score').textContent = '73';
+// document.querySelector('.number').textContent = '23';
+// document.querySelector('.score').textContent = '73';
 
-console.log(document.querySelector('.guess').nodeValue);
+// console.log(document.querySelector('.guess').nodeValue);
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess);
+})

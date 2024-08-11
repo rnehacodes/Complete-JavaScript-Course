68ebac3383f9f8fe5d6aa62145a841d2d11d1230
Neha Rajput <neharajputcoding@gmail.com>
2024-08-11 22:01:54 +0530

    Hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,11 +1,46 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-    console.log(Name);
-    return age;
-}
+//Scoping
+let output = 'test';
+console.log(output);
 
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  console.log(output);
+
+  output = 'updated';
+
+  console.log(output);
+}
 
 const firstName = 'Jonas';
 calcAge(1991);
+console.log(output);
+
+
+//Hoisting
+
+//Variable Hoisting
+console.log(thing);
+console.log(color);
+console.log(category);
+
+var thing = 'Apple';
+let color = 'red';
+const category = 'fruit';
+
+//Functions Hoisting
+console.log(addNums(4, 5));
+console.log(addVals(4, 5));
+console.log(addArrow(4, 5));
+
+function addNums(a, b) {
+  return a + b;
+}
+
+var addVals = function(a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;

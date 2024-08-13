a40334ee60326508b0b3bbd3730bf7a2bf24d1a8
Neha Rajput <neharajputcoding@gmail.com>
2024-08-13 23:54:13 +0530

    this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,46 +1,90 @@
 'use strict';
 
 //Scoping
-let output = 'test';
-console.log(output);
+// let output = 'test';
+// console.log(output);
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  console.log(firstName);
-  console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   console.log(firstName);
+//   console.log(output);
 
-  output = 'updated';
+//   output = 'updated';
 
-  console.log(output);
+//   console.log(output);
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// console.log(output);
+
+
+// //Hoisting
+
+// //Variable Hoisting
+// console.log(thing);
+// console.log(color);
+// console.log(category);
+
+// var thing = 'Apple';
+// let color = 'red';
+// const category = 'fruit';
+
+// //Functions Hoisting
+// console.log(addNums(4, 5));
+// console.log(addVals(4, 5));
+// console.log(addArrow(4, 5));
+
+// function addNums(a, b) {
+//   return a + b;
+// }
+
+// var addVals = function(a, b) {
+//   return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+//this keyword
+console.log(this);
+
+//Normal Function
+const sumNums = function(a, b) {
+    console.log(a + b);
+    console.log(this);
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
-console.log(output);
+sumNums(4, 6);
 
-
-//Hoisting
-
-//Variable Hoisting
-console.log(thing);
-console.log(color);
-console.log(category);
-
-var thing = 'Apple';
-let color = 'red';
-const category = 'fruit';
-
-//Functions Hoisting
-console.log(addNums(4, 5));
-console.log(addVals(4, 5));
-console.log(addArrow(4, 5));
-
-function addNums(a, b) {
-  return a + b;
+//Arrow Function
+const diffNums = (x, y) => {
+    console.log(x - y);
+    console.log(this);
 }
 
-var addVals = function(a, b) {
-  return a + b;
+diffNums(6, 8);
+
+const employee = {
+    employeeName : 'Anna',
+    ID : 45,
+    getID : function () {
+        console.log("ID: " + this.ID);
+        console.log(this);
+    },
+    getName : function() {
+        console.log("Name: " + this.employeeName);
+    }
 }
 
-var addArrow = (a, b) => a + b;
+employee.getID();
+
+const client = {
+    clientName : "Ditto",
+    ID : 77
+} 
+
+console.log(client);
+
+client.getID = employee.getID;
+
+client.getID();

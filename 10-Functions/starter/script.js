231a1c23770b3fce2bb489a6eb5b8578a75d5994
Neha Rajput <neharajputcoding@gmail.com>
2024-10-03 23:46:28 +0530

    Functions: Default Parameters & Arguments Passing Value vs Reference

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//Function: Default Parameters
+/* console.log('Function: Default Parameters'.padStart(30, '~').padEnd('32', '~'));
+
+const bookings = [];
+
+function createBooking (flightName, passengers = 40, seats = passengers * 1.5) {
+    const booking = {
+        flightName,
+        passengers,
+        seats
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('Airbus 320', 45, 65);
+createBooking('Boeing 950');
+console.log(...bookings); */
+
+//Function: Arguments Passing - Value vs Reference
+console.log('Function: Arguments Passing - Value vs Reference'.padStart(35, '~').padEnd(37, '~'));
+
+const passenger = {name : 'Neha Rajput', bookingID : 21051}, flight = 'ATR 72B';
+
+const confirmedBookings = [{passengerName : 'Neha Rajput', PNR : 21051}];
+
+function verifyBooking (flightName, passengerDetails) {
+    flightName = flightName + ' will take off from Terminal 2!';
+    if(confirmedBookings[0].PNR == passengerDetails.bookingID) {
+        passengerDetails.bookingStatus = `Your booking with booking ID ${passengerDetails.bookingID} has been confirmed!`
+    } else {
+        passengerDetails.bookingStatus = `Your booking with booking ID ${passengerDetails.bookingID} has NOT been confirmed!`
+    }
+    console.log(`Dear ${passengerDetails.name},\n${passengerDetails.bookingStatus}`);
+}
+
+console.log(flight, passenger);
+verifyBooking(flight, passenger);
+console.log(flight, passenger);
